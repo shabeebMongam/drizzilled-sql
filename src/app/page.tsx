b01f0158f1aac4472008a34b3b5e063e7269e483
shabeeb <shabeebmongam@gmail.com>
2025-02-15 20:15:09 +0530

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
 
-export default function Home() {
+const mockUrls = [
+  "https://k3kf98rd3x.ufs.sh/f/J8B8pafVpZXlokBPWRbu0PCpJoIWjzDB7FY1d6aTxgEeStUf",
+  "https://k3kf98rd3x.ufs.sh/f/J8B8pafVpZXlcp5tQTCAeXMs2KITqtfmUHLZvNzboE4yYnd0",
+  "https://k3kf98rd3x.ufs.sh/f/J8B8pafVpZXlzbWUSXIhe4FyhWJbjoAXfn3lqdawir10Vck7",
+  "https://k3kf98rd3x.ufs.sh/f/J8B8pafVpZXlCJfPdRAPRpN8jKfWv0LuZnyXgwM4l1UAHe2x"
+]
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }))
+
+export default function HomePage() {
   return (
-   <div>
-    Hello
-   </div>
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {
+          [...mockImages, ...mockImages, ...mockImages].map((image) => (
+            <div key={image.id} className="w-48 ">
+              <img src={image.url} alt="image" />
+            </div>
+          )
+          )
+        }
+      </div>
+
+    </main>
 
 
   );
